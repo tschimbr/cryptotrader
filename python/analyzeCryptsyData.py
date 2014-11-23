925d4a40012950971f835fe20693f7153a432878
Tim Peter <tim.peter@eonum.ch>
2014-11-23 19:59:49 +0100

    analyze currencies after launch on cryptsy.

--- python/analyzeCryptsyData.py
+++ python/analyzeCryptsyData.py
@@ -9,6 +9,9 @@ print "Analyzing data from folder " + folder
 os.chdir(folder)
 archive_files = sorted([f for f in os.listdir(folder) if f.endswith('.tar.gz')])
 
+analysis = {}
+one_day_average = {}
+
 for file in archive_files:
 	print "Analyzing tar.gz file: " + file
 	os.system("rm -R ALL_MARKETS")
@@ -16,10 +19,26 @@ for file in archive_files:
 	json_files = sorted([f for f in os.listdir("ALL_MARKETS") if f.endswith('.json')])
 	for json_file in json_files:
 		print "Analyzing JSON file: " + json_file
+		json_data = open("ALL_MARKETS/" + json_file)
+		market_info = json.load(json_data)
+		markets = market_info['return']['markets']
+		market_keys = markets.keys()
+		pprint(markets[market_keys[0]])
+		
+		json_data.close()
+		exit()
+		
 
 
-#json_data=open('json_data')
+# print analysis to file
+markets = analysis.keys()
 
-#data = json.load(json_data)
-#pprint(data)
-#json_data.close()
+output = open(sys.argv[2], 'w')
+for market in markets:
+	output.write(market + ";")
+	for day in markets[markets]:
+		output.write(day + ";")
+output.writeln()
+
+
+output.close()
